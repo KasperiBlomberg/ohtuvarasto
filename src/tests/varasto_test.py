0d8d4cda58a2103eb06daf0e3b5eb1ecd982eb71
KasperiBlomberg <blomberg.kasperi@gmail.com>
2024-10-28 20:53:09 +0200

    unittest

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_varastoon_lisataan_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_varastosta_otetaan_liikaa(self):
+        self.varasto.ota_varastosta(1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_luodaan_varasto_negatiivisella_tilavuudella(self):
+        self.varasto = Varasto(-1)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_luodaan_varasto_negatiivisella_saldolla(self):
+        self.varasto = Varasto(10, -1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisataan_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_ota_negatiivinen_maara(self):
+        saatu_maara = self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_tulostus_toimii(self):
+        self.varasto.lisaa_varastoon(6)
+
+        self.assertEqual(str(self.varasto), "saldo = 6, vielä tilaa 4")
